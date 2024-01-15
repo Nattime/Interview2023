d7b564ce4b34c4c46989abb9bbb8896d25123b70
Jie Huang <5761938+Nattime@users.noreply.github.com>
2024-01-15 18:44:23 -0500

    added BracketsToParentheses to convert inputs with [] to {}

---
+++ src/resources/BracketsToParentheses.java
@@ -0,0 +1,18 @@
+package resources;
+
+public class BracketsToParentheses{
+    public String convert(String str){
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '['){
+                ret.append("{");
+            }else if(ch == ']'){
+                ret.append("}");
+            }else{
+                ret.append(ch);
+            }
+        }
+        return ret.toString();
+    }
+}
